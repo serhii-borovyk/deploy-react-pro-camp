cff081457fddbb54db9c2e01a1bd9bf63e150007
serhii.borovyk <serhii.borovyk@globallogic.com>
2019-03-19 17:53:00 +0200

    Updates

---
+++ precache-manifest.3e3f75ba362d97e51cc4c68ee8ead2d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0c4e8d64904b02a0909bee104d179c",
+    "url": "/deploy-react-pro-camp/static/media/poster_none.4c0c4e8d.png"
+  },
+  {
+    "revision": "1c36d7a660a074cc9423",
+    "url": "/deploy-react-pro-camp/static/js/runtime~main.848d2136.js"
+  },
+  {
+    "revision": "1fd0eaf4c7d54d2bd5cc",
+    "url": "/deploy-react-pro-camp/static/js/main.b9deaf5d.chunk.js"
+  },
+  {
+    "revision": "f49fa14d8ecf6b10bf74",
+    "url": "/deploy-react-pro-camp/static/js/2.132ef98d.chunk.js"
+  },
+  {
+    "revision": "1818f0f0849ff03f2928a4791f237666",
+    "url": "/deploy-react-pro-camp/index.html"
+  }
+];
